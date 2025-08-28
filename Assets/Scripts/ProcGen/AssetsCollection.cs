1ce27a2fd89d2a59ef0922c69e380a735610467f
NeoThePotato <neorayb@gmail.com>
2025-08-28 19:49:06 +0300

    Add more fields to AssetsCollection.

--- Assets/Scripts/ProcGen/AssetsCollection.cs
+++ Assets/Scripts/ProcGen/AssetsCollection.cs
@@ -8,12 +8,16 @@ namespace ProcGen
 	{
 		[Header("Walls")]
 		[SerializeField] private GameObject[] _walls;
+		[SerializeField] private GameObject[] _doors;
+		[SerializeField] private GameObject[] _windows;
 		[SerializeField] private Material[] _wallMaterials;
 		[SerializeField] private Material[] _floorMaterials;
 		[Header("Furniture")]
 		[SerializeField] private GameObject[] _furniture;
 
 		public IReadOnlyList<GameObject> Walls => _walls;
+		public IReadOnlyList<GameObject> Doors => _doors;
+		public IReadOnlyList<GameObject> Windows => _windows;
 		public IReadOnlyList<Material> WallMaterials => _wallMaterials;
 		public IReadOnlyList<Material> FloorMaterials => _floorMaterials;
 		public IReadOnlyList<GameObject> Furniture => _furniture;
