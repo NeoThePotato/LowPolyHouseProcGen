ada79f873121a74b5c425ee75a3094ecb22aac8b
NeoThePotato <neorayb@gmail.com>
2025-08-29 21:05:46 +0300

    Implement Binary-Space-Partitioning algorithm.

--- Assets/Scripts/ProcGen/Generator/BSP.cs
+++ Assets/Scripts/ProcGen/Generator/BSP.cs
@@ -1,10 +1,86 @@
-﻿namespace ProcGen
+﻿using UnityEngine;
+using Unity.Mathematics;
+using Unity.Mathematics.Geometry;
+using random = Unity.Mathematics.Random;
+using ProcGen.Collections;
+
+namespace ProcGen
 {
 	public static partial class Generator
 	{
 		public static class BSP
 		{
+			public static void SplitRecursive(in Input input, ref random random, INode<RoomData> root)
+			{
+				if (!TrySplitSingle(input, ref random, root))
+					return;
+				SplitRecursive(input, ref random, root.Left);
+				SplitRecursive(input, ref random, root.Right);
+			}
 
+			public static bool TrySplitSingle(in Input input, ref random random, INode<RoomData> toSplit)
+			{
+				const string LEFT = "left", RIGHT = "Right", DOWN = "Down", UP = "UP";
+
+				var roomToSplit = toSplit.Value;
+				ref readonly var boundingVolume = ref roomToSplit.boundingVolume;
+				if (!CanSplit(in input.roomSize.min, boundingVolume, out var result))
+					return false;
+				SplitAlongRandomAxis(boundingVolume, in result, ref random, out var boundsLeft, out var boundsRight, out var horizontal);
+				toSplit.Left = CreateNode(roomToSplit, Zip(in boundingVolume, in boundsLeft), horizontal ? LEFT : DOWN);
+				toSplit.Right = CreateNode(roomToSplit, Zip(in boundingVolume, in boundsRight), horizontal ? RIGHT : UP);
+				return true;
+			}
+
+			private static void SplitAlongRandomAxis(in MinMax2 boundingVolume, in SplitRanges split, ref random random, out MinMax2 left, out MinMax2 right, out bool horizontal)
+			{
+				right = left = boundingVolume;
+				if (horizontal = Horizontal(split.PossibleDirections, ref random))
+					left.max.x = right.min.x = random.Random(split.X);
+				else
+					left.max.y = right.min.y = random.Random(split.Y);
+
+				static bool Horizontal(bool2 possibleDirections, ref random random)
+				{
+					if (math.all(possibleDirections))
+						return random.NextBool();
+					else
+						return possibleDirections.x;
+				}
+			}
+
+			private static bool CanSplit(in float2 minRoomSize, in MinMax2 bounds, out SplitRanges result)
+			{
+				result.ranges = new(bounds.min + minRoomSize, bounds.max - minRoomSize);
+				return result;
+			}
+
+			private static MinMaxAABB Zip(in MinMaxAABB source, in MinMax2 xy) => new(new(xy.min, source.Min.z), new(xy.max, source.Max.z));
+
+			private static INode<RoomData> CreateNode(Transform parent, MinMaxAABB bounds, string name)
+			{
+				var child = new GameObject(parent.name + '-' + name).transform;
+				child.transform.parent = parent;
+				return new BinaryTree<RoomData>.Node()
+				{
+					Value = new(bounds, child)
+				};
+			}
+
+			private struct SplitRanges
+			{
+				public MinMax2 ranges;
+
+				public readonly MinMax X => new(ranges.min.x, ranges.max.x);
+				public readonly MinMax Y => new(ranges.min.y, ranges.max.y);
+				public readonly bool2 PossibleDirections => ranges.Range >= 0f;
+
+				public static implicit operator SplitRanges(in MinMax2 ranges) => new() { ranges = ranges };
+
+				public static implicit operator MinMax2(in SplitRanges split) => split.ranges;
+
+				public static implicit operator bool(in SplitRanges split) => math.any(split.PossibleDirections);
+			}
 		}
 	}
 }
