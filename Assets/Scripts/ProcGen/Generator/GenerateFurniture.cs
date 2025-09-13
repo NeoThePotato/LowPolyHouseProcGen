735948ce914c55c359e5f49629ad51634ca65107
LehMipo412 <barbetzalel412@gmail.com>
2025-09-13 17:49:24 +0300

    seperated the functions of wich furniture to spawn but there are bugs with the number of room

--- Assets/Scripts/ProcGen/Generator/GenerateFurniture.cs
+++ Assets/Scripts/ProcGen/Generator/GenerateFurniture.cs
@@ -5,6 +5,7 @@ using random = Unity.Mathematics.Random;
 using ProcGen.Collections;
 using Unity.Mathematics;
 using UnityEditor;
+using System.Linq;
 
 namespace ProcGen
 {
@@ -20,9 +21,41 @@ namespace ProcGen
         /// <param name="rooms">Rooms to furnish.</param>
         public static void GenerateFurniture(in Input input, ref random random, IEnumerable<INode<RoomData>> rooms, Dictionary<INode<RoomData>, RoomType> roomTypes)
 		{
+
+			//Debug.Log(rooms.Count());
+			//Debug.Log(roomTypes.Count());
 			// TODO talk with nehorai about the duplicated lists
 			foreach (var room in rooms)
-				GenerateFurniture(room.Value, input.assets.Furniture, ref random, roomTypes);
+            {
+				
+
+					
+				switch (roomTypes[room])
+                {
+					case RoomType.KeyRoom:
+						PlaceKeyRoomObjects(room.Value, input.assets.Furniture, ref random, roomTypes);
+							Debug.LogWarning("KEyRoom");
+						break;
+
+					case RoomType.LockedRoom:
+						PlaceLockedRoomObjects(room.Value, input.assets.Furniture, ref random, roomTypes);
+						Debug.LogWarning("DoorRoom");
+						break;
+
+					case RoomType.None:
+                         GenerateFurniture(room.Value, input.assets.Furniture, ref random, roomTypes);
+                         break;
+
+					default:
+						GenerateFurniture(room.Value, input.assets.Furniture, ref random, roomTypes);
+							
+							break;
+
+
+
+					}
+			}
+				
 		}
 
 		/// <summary>
@@ -33,7 +66,7 @@ namespace ProcGen
 		/// <param name="random">Random number generator.</param>
 		public static void GenerateFurniture(in RoomData roomBounds, IReadOnlyList<GameObject> furniture, ref random random, Dictionary<INode<RoomData>, RoomType> roomTypes)
 		{
-			GameObject go = assets.KeyRoomObjects[random.NextInt(0, assets.KeyRoomObjects.Count)];
+			GameObject go = assets.Furniture[random.NextInt(0, assets.Furniture.Count)];
 			go.tag = "Furniture";
 
             Vector3 position = random.NextFloat3(roomBounds.boundingVolume.Min, roomBounds.boundingVolume.Max);
@@ -41,14 +74,24 @@ namespace ProcGen
             GameObject.Instantiate(go, position, rotation, roomBounds.parent);
         }
 
-		public static void PlaceKeyRoomObjects()
+		public static void PlaceKeyRoomObjects(in RoomData roomBounds, IReadOnlyList<GameObject> furniture, ref random random, Dictionary<INode<RoomData>, RoomType> roomTypes)
 		{
-            // TODO implement
-        }
+			GameObject go = assets.KeyRoomObjects[random.NextInt(0, assets.KeyRoomObjects.Count)];
+			go.tag = "Furniture";
 
-		public static void PlaceLockedRoomObjects()
+			Vector3 position = random.NextFloat3(roomBounds.boundingVolume.Min, roomBounds.boundingVolume.Max);
+			quaternion rotation = quaternion.Euler(random.NextFloat3(0, 360f) * math.up());
+			GameObject.Instantiate(go, position, rotation, roomBounds.parent);
+		}
+
+		public static void PlaceLockedRoomObjects(in RoomData roomBounds, IReadOnlyList<GameObject> furniture, ref random random, Dictionary<INode<RoomData>, RoomType> roomTypes)
 		{
-            // TODO implement
-        }
+			GameObject go = assets.LockedRoomObjects[random.NextInt(0, assets.LockedRoomObjects.Count)];
+			go.tag = "Furniture";
+
+			Vector3 position = random.NextFloat3(roomBounds.boundingVolume.Min, roomBounds.boundingVolume.Max);
+			quaternion rotation = quaternion.Euler(random.NextFloat3(0, 360f) * math.up());
+			GameObject.Instantiate(go, position, rotation, roomBounds.parent);
+		}
     }
 }
