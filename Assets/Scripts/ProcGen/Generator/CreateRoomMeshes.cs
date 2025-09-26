77ad3930464d27501e73af589e21d340534c60a6
NeoThePotato <neorayb@gmail.com>
2025-09-26 08:27:44 +0300

    Bind UVs to generated room meshes.

--- Assets/Scripts/ProcGen/Generator/CreateRoomMeshes.cs
+++ Assets/Scripts/ProcGen/Generator/CreateRoomMeshes.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering;
 using Unity.Mathematics;
 using Unity.Mathematics.Geometry;
 using random = Unity.Mathematics.Random;
-using UnityEngine;
-using UnityEngine.Rendering;
 
 namespace ProcGen
 {
@@ -34,22 +35,30 @@ namespace ProcGen
 
 		private static Mesh CreateMesh(RoomData room)
 		{
+			const int UV_CHANNEL = 0;
+
 			var volume = room.boundingVolume;
 			CenterBounds(ref volume);
 			Mesh mesh = new();
 			List<Vector3> vertices = new();
 			List<int> quads = new();
+			List<Vector2> uv = new();
 
 			var floor = CreateFloor(in volume, vertices, quads);
 			var ceiling = CreateCeiling(in volume, vertices, quads);
 			var walls = CreateWalls(vertices, quads);
+			uv.AddRange(FloorCeilingUVs(vertices, Combine(floor, ceiling)));
+			uv.AddRange(WallUVs(vertices, walls));
 			mesh.subMeshCount = SUBMESH_COUNT;
 			mesh.SetVertices(vertices);
-			mesh.SetIndices(quads, floor.indexStart, floor.indexCount, MeshTopology.Quads, FLOOR, true);
-			mesh.SetIndices(quads, ceiling.indexStart, ceiling.indexCount, MeshTopology.Quads, CEILING, true);
-			mesh.SetIndices(quads, walls.indexStart, walls.indexCount, MeshTopology.Quads, WALLS, true);
+			mesh.SetUVs(UV_CHANNEL, uv);
+			//mesh.SetSubMesh(); // TODO Try this instead of the 3 lines below
+			mesh.SetIndices(quads, floor.indexStart, floor.indexCount, floor.topology, FLOOR, false);
+			mesh.SetIndices(quads, ceiling.indexStart, ceiling.indexCount, ceiling.topology, CEILING, false);
+			mesh.SetIndices(quads, walls.indexStart, walls.indexCount, walls.topology, WALLS, false);
 			mesh.RecalculateBounds();
 			mesh.RecalculateNormals();
+			mesh.RecalculateTangents();
 			return mesh;
 		}
 
@@ -95,12 +104,44 @@ namespace ProcGen
 
 		private static SubMeshDescriptor CreateWalls(List<Vector3> vertices, List<int> quads)
 		{
-			SubMeshDescriptor walls = new(indexStart: vertices.Count, indexCount: 4*4, topology: MeshTopology.Quads);
-			quads.AddRange(LEFT);
-			quads.AddRange(UP);
-			quads.AddRange(RIGHT);
-			quads.AddRange(DOWN);
-			return walls;
+			var left = Wall(vertices, quads, LEFT);
+			var up = Wall(vertices, quads, UP);
+			var right = Wall(vertices, quads, RIGHT);
+			var down = Wall(vertices, quads, DOWN);
+			return Combine(left, up, right, down);
+
+			static SubMeshDescriptor Wall(List<Vector3> vertices, List<int> quads, int[] copy)
+			{
+				SubMeshDescriptor wall = new(indexStart: vertices.Count, indexCount: copy.Length, topology: MeshTopology.Quads);
+				for (int i = 0; i < copy.Length; i++)
+					vertices.Add(vertices[copy[i]]);
+				for (int i = 0; i < wall.indexCount; i++)
+					quads.Add(wall.indexStart + i);
+				return wall;
+			}
+		}
+
+		private static IEnumerable<Vector2> FloorCeilingUVs(List<Vector3> vertices, SubMeshDescriptor submesh)
+		{
+			return vertices.GetRange(submesh.indexStart, submesh.indexCount).Select(v => new Vector2(v.x, v.z));
+		}
+
+		private static IEnumerable<Vector2> WallUVs(List<Vector3> vertices, SubMeshDescriptor submesh)
+		{
+			return vertices.GetRange(submesh.indexStart, submesh.indexCount).Select(v => new Vector2(v.x + v.z, v.y)); // TODO Refine
+		}
+
+		private static SubMeshDescriptor Combine(SubMeshDescriptor d1, SubMeshDescriptor d2)
+		{
+			return new(math.min(d1.indexStart, d2.indexStart), d1.indexCount + d2.indexCount, topology: d1.topology);
+		}
+
+		private static SubMeshDescriptor Combine(SubMeshDescriptor d1, SubMeshDescriptor d2, SubMeshDescriptor d3, SubMeshDescriptor d4)
+		{
+			return new(
+				math.min(math.min(d1.indexStart, d2.indexStart), math.min(d3.indexStart, d4.indexStart)),
+				d1.indexCount + d2.indexCount + d3.indexCount + d4.indexCount,
+				topology: d1.topology);
 		}
 
 		private static void CenterBounds(ref MinMaxAABB bounds)
