4bb97f99bd415967a927a9d70683655ae232c2ce
NeoThePotato <neorayb@gmail.com>
2025-09-26 09:25:53 +0300

    Optimize and streamline CreateRoomMeshes.

--- Assets/Scripts/ProcGen/Generator/CreateRoomMeshes.cs
+++ Assets/Scripts/ProcGen/Generator/CreateRoomMeshes.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using UnityEngine;
 using UnityEngine.Rendering;
 using Unity.Mathematics;
 using Unity.Mathematics.Geometry;
-using random = Unity.Mathematics.Random;
+using Unity.Collections;
 
 namespace ProcGen
 {
@@ -18,7 +17,7 @@ namespace ProcGen
 			RIGHT = { 2, 6, 7, 3 },
 			DOWN = { 3, 7, 4, 0 };
 
-		public static void CreateRoomMeshes(in Input input, ref random random, IEnumerable<RoomData> rooms)
+		public static void CreateRoomMeshes(RoomData[] rooms)
 		{
 			foreach (var room in rooms)
 				CreateAndAssignMesh(room);
@@ -36,35 +35,44 @@ namespace ProcGen
 		private static Mesh CreateMesh(RoomData room)
 		{
 			const int UV_CHANNEL = 0;
+			const MeshUpdateFlags DONT_UPDATE = MeshUpdateFlags.DontValidateIndices | MeshUpdateFlags.DontResetBoneBounds | MeshUpdateFlags.DontNotifyMeshUsers | MeshUpdateFlags.DontRecalculateBounds;
 
 			var volume = room.boundingVolume;
 			CenterBounds(ref volume);
 			Mesh mesh = new();
-			List<Vector3> vertices = new();
-			List<int> quads = new();
-			List<Vector2> uv = new();
+			using NativeList<Vector3> vertices = new(Allocator.Temp);
+			using NativeList<ushort> quads = new(Allocator.Temp);
+			using NativeList<Vector2> uv = new(Allocator.Temp);
+			using NativeArray<SubMeshDescriptor> subMeshes = new(SUBMESH_COUNT, Allocator.Temp);
 
-			var floor = CreateFloor(in volume, vertices, quads);
-			var ceiling = CreateCeiling(in volume, vertices, quads);
-			var walls = CreateWalls(vertices, quads);
-			uv.AddRange(FloorCeilingUVs(vertices, Combine(floor, ceiling)));
-			uv.AddRange(WallUVs(vertices, walls));
-			mesh.subMeshCount = SUBMESH_COUNT;
-			mesh.SetVertices(vertices);
-			mesh.SetUVs(UV_CHANNEL, uv);
-			//mesh.SetSubMesh(); // TODO Try this instead of the 3 lines below
-			mesh.SetIndices(quads, floor.indexStart, floor.indexCount, floor.topology, FLOOR, false);
-			mesh.SetIndices(quads, ceiling.indexStart, ceiling.indexCount, ceiling.topology, CEILING, false);
-			mesh.SetIndices(quads, walls.indexStart, walls.indexCount, walls.topology, WALLS, false);
-			mesh.RecalculateBounds();
+			// Create Vertices & Quads
+			ref SubMeshDescriptor floor = ref subMeshes.AsSpan()[FLOOR];
+			ref SubMeshDescriptor ceiling = ref subMeshes.AsSpan()[CEILING];
+			ref SubMeshDescriptor walls = ref subMeshes.AsSpan()[WALLS];
+			floor = CreateFloor(in volume, vertices, quads);
+			ceiling = CreateCeiling(in volume, vertices, quads);
+			walls = CreateWalls(vertices, quads);
+
+			// Create UVs
+			FloorCeilingUVs(SpanFromDescriptor(vertices, Combine(floor, ceiling)), uv);
+			WallUVs(SpanFromDescriptor(vertices, walls), uv);
+
+			// Set Mesh data
+			mesh.SetVertices(vertices.AsArray(), 0, vertices.Length, DONT_UPDATE);
+			mesh.SetIndexBufferParams(quads.Length, IndexFormat.UInt16);
+			mesh.SetIndexBufferData(quads.AsArray(), 0, 0, quads.Length, DONT_UPDATE);
+			mesh.SetUVs(UV_CHANNEL, uv.AsArray(), 0, uv.Length, DONT_UPDATE);
+			mesh.SetSubMeshes(subMeshes);
+
+			// Validate mesh
 			mesh.RecalculateNormals();
 			mesh.RecalculateTangents();
 			return mesh;
 		}
 
-		private static SubMeshDescriptor CreateFloor(in MinMaxAABB bounds, List<Vector3> vertices, List<int> quads)
+		private static SubMeshDescriptor CreateFloor(in MinMaxAABB bounds, NativeList<Vector3> vertices, NativeList<ushort> quads)
 		{
-			SubMeshDescriptor floor = new(indexStart: vertices.Count, indexCount: 4, topology: MeshTopology.Quads);
+			SubMeshDescriptor floor = new(indexStart: vertices.Length, indexCount: 4, topology: MeshTopology.Quads);
 			float
 				y = bounds.Min.y,
 				left = bounds.Min.x,
@@ -75,16 +83,16 @@ namespace ProcGen
 			vertices.Add(new(left, y, top));
 			vertices.Add(new(right, y, top));
 			vertices.Add(new(right, y, bottom));
-			quads.Add(floor.indexStart);
-			quads.Add(floor.indexStart + 1);
-			quads.Add(floor.indexStart + 2);
-			quads.Add(floor.indexStart + 3);
+			quads.Add((ushort)floor.indexStart);
+			quads.Add((ushort)(floor.indexStart + 1));
+			quads.Add((ushort)(floor.indexStart + 2));
+			quads.Add((ushort)(floor.indexStart + 3));
 			return floor;
 		}
 
-		private static SubMeshDescriptor CreateCeiling(in MinMaxAABB bounds, List<Vector3> vertices, List<int> quads)
+		private static SubMeshDescriptor CreateCeiling(in MinMaxAABB bounds, NativeList<Vector3> vertices, NativeList<ushort> quads)
 		{
-			SubMeshDescriptor ceiling = new(indexStart: vertices.Count, indexCount: 4, topology: MeshTopology.Quads);
+			SubMeshDescriptor ceiling = new(indexStart: vertices.Length, indexCount: 4, topology: MeshTopology.Quads);
 			float
 				y = bounds.Max.y,
 				left = bounds.Min.x,
@@ -95,14 +103,14 @@ namespace ProcGen
 			vertices.Add(new(left, y, top));
 			vertices.Add(new(right, y, top));
 			vertices.Add(new(right, y, bottom));
-			quads.Add(ceiling.indexStart);
-			quads.Add(ceiling.indexStart + 3);
-			quads.Add(ceiling.indexStart + 2);
-			quads.Add(ceiling.indexStart + 1);
+			quads.Add((ushort)ceiling.indexStart);
+			quads.Add((ushort)(ceiling.indexStart + 3));
+			quads.Add((ushort)(ceiling.indexStart + 2));
+			quads.Add((ushort)(ceiling.indexStart + 1));
 			return ceiling;
 		}
 
-		private static SubMeshDescriptor CreateWalls(List<Vector3> vertices, List<int> quads)
+		private static SubMeshDescriptor CreateWalls(NativeList<Vector3> vertices, NativeList<ushort> quads)
 		{
 			var left = Wall(vertices, quads, LEFT);
 			var up = Wall(vertices, quads, UP);
@@ -110,25 +118,36 @@ namespace ProcGen
 			var down = Wall(vertices, quads, DOWN);
 			return Combine(left, up, right, down);
 
-			static SubMeshDescriptor Wall(List<Vector3> vertices, List<int> quads, int[] copy)
+			static SubMeshDescriptor Wall(NativeList<Vector3> vertices, NativeList<ushort> quads, int[] copy)
 			{
-				SubMeshDescriptor wall = new(indexStart: vertices.Count, indexCount: copy.Length, topology: MeshTopology.Quads);
+				SubMeshDescriptor wall = new(indexStart: vertices.Length, indexCount: copy.Length, topology: MeshTopology.Quads);
 				for (int i = 0; i < copy.Length; i++)
 					vertices.Add(vertices[copy[i]]);
 				for (int i = 0; i < wall.indexCount; i++)
-					quads.Add(wall.indexStart + i);
+					quads.Add((ushort)(wall.indexStart + i));
 				return wall;
 			}
 		}
 
-		private static IEnumerable<Vector2> FloorCeilingUVs(List<Vector3> vertices, SubMeshDescriptor submesh)
+		private static void FloorCeilingUVs(ReadOnlySpan<Vector3> vertices, NativeList<Vector2> uv)
 		{
-			return vertices.GetRange(submesh.indexStart, submesh.indexCount).Select(v => new Vector2(v.x, v.z));
+			for (int i = 0; i < vertices.Length; i++)
+				uv.Add(ToUV(vertices[i]));
+
+			static Vector2 ToUV(Vector3 vertex) => new(vertex.x, vertex.z);
 		}
 
-		private static IEnumerable<Vector2> WallUVs(List<Vector3> vertices, SubMeshDescriptor submesh)
+		private static void WallUVs(ReadOnlySpan<Vector3> vertices, NativeList<Vector2> uv)
 		{
-			return vertices.GetRange(submesh.indexStart, submesh.indexCount).Select(v => new Vector2(v.x + v.z, v.y)); // TODO Refine
+			for (int i = 0; i < vertices.Length; i++)
+				uv.Add(ToUV(vertices[i]));
+
+			static Vector2 ToUV(Vector3 vertex) => new(vertex.x + vertex.z, vertex.y);
+		}
+
+		private static Span<T> SpanFromDescriptor<T>(NativeList<T> source, SubMeshDescriptor descriptor) where T : unmanaged
+		{
+			return source.AsArray().AsSpan().Slice(descriptor.indexStart, descriptor.indexCount);
 		}
 
 		private static SubMeshDescriptor Combine(SubMeshDescriptor d1, SubMeshDescriptor d2)
